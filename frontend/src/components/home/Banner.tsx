c1d5f137a1f72dcf46fc68e7e0377683499abc6d
Ajeet Patel <patelajeet1606@gmail.com>
2024-08-11 22:10:52 +0530

    delay in API response handled.

--- frontend/src/components/home/Banner.tsx
+++ frontend/src/components/home/Banner.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import { useGetBannersQuery } from "../../redux/api/bannerSlice";
-const Banner = () => {
-  const { data: banners } = useGetBannersQuery({ page: 1, limit: 10 });
+import { BannerType } from "./data";
 
-  const [count, setCount] = useState(banners?.data[0].timer || 0);
+interface props {
+  banners: BannerType[];
+}
+const Banner: React.FC<props> = ({ banners }) => {
+  const [count, setCount] = useState(banners[0].timer || 0);
   const [currentBanner, setCurrentBanner] = useState(0);
-  const noCardsToShow: boolean = banners?.data.some((card) => card.visible)
+  const noCardsToShow: boolean = banners.some((card) => card.visible)
     ? false
     : true; // Check if any card is visible
 
   useEffect(() => {
-    if (banners?.data == undefined || banners?.data.length == 0) return;
+    if (banners == undefined || banners.length == 0) return;
     if (count > 0) {
       const timerId = setTimeout(() => {
         setCount(count - 1);
@@ -18,12 +20,12 @@ const Banner = () => {
 
       return () => clearTimeout(timerId);
     } else {
-      let next = (currentBanner + 1) % banners?.data.length;
-      while (banners?.data[next].visible === false) {
-        next = (next + 1) % banners.data.length;
+      let next = (currentBanner + 1) % banners.length;
+      while (banners[next].visible === false) {
+        next = (next + 1) % banners.length;
       }
       setCurrentBanner(next);
-      setCount(banners?.data[next].timer);
+      setCount(banners[next].timer);
     }
   }, [count]);
 
@@ -35,7 +37,7 @@ const Banner = () => {
         </div>
       ) : (
         <a
-          href={banners?.data[currentBanner].link}
+          href={banners[currentBanner].link}
           target="_blank"
           className="w-[90%] md:w-2/3"
         >
@@ -44,20 +46,20 @@ const Banner = () => {
             key={currentBanner}
           >
             <img
-              src={banners?.data[currentBanner].image}
+              src={banners[currentBanner].image}
               alt=""
               className="w-[150px] h-[150px] rounded-md float-left mr-2 mb-2"
             />
             <div className="flex justify-between items-center gap-2">
               <h3 className="text-3xl font-bold">
-                {banners?.data[currentBanner].name}
+                {banners[currentBanner].name}
               </h3>
               <span className="text-xl inset-0 animate-slide-down" key={count}>
                 {count}
               </span>
             </div>
             <p className="md:text-lg text-md text-base leading-relaxed">
-              {banners?.data[currentBanner].about}
+              {banners[currentBanner].about}
             </p>
           </div>
         </a>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import Banner from "../components/home/Banner";
 import { NavLink } from "react-router-dom";
 import { Button } from "antd";
+import { useGetBannersQuery } from "../redux/api/bannerSlice";
 
 const Home = () => {
+  const { data: banners } = useGetBannersQuery({ page: 1, limit: 10 });
+
+  if(!banners) return;
   return (
     <div className="flex flex-col justify-center items-center gap-4 w-full h-[100vh]">
-      <Banner />
+      <Banner banners={banners?.data}/>
 
       <NavLink to="/dashboard">
         <Button className="border shadow-md rounded-md mx-auto bg-[#f5f5f5] p-3 font-montserrat cursor-pointer h-9">
