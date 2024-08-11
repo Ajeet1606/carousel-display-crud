52b7894887c34dd1b9bc72969a4ca25cec20ab04
Ajeet Patel <patelajeet1606@gmail.com>
2024-08-11 22:36:35 +0530

    API failure handled.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,10 +6,16 @@ import { useGetBannersQuery } from "../redux/api/bannerSlice";
 const Home = () => {
   const { data: banners } = useGetBannersQuery({ page: 1, limit: 10 });
 
-  if(!banners) return;
+  if (!banners)
+    return (
+      <div className="w-2/3 border shadow-md rounded-md mx-auto bg-[#f5f5f5] hover:shadow-2xl p-3 font-montserrat">
+        <h3>Hold on, we're fetching all the cards.. 😋</h3>
+      </div>
+    );
+
   return (
     <div className="flex flex-col justify-center items-center gap-4 w-full h-[100vh]">
-      <Banner banners={banners?.data}/>
+      <Banner banners={banners?.data} />
 
       <NavLink to="/dashboard">
         <Button className="border shadow-md rounded-md mx-auto bg-[#f5f5f5] p-3 font-montserrat cursor-pointer h-9">
