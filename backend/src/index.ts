9f56423608b07a88633ded9b59ce7f391911df32
Ajeet Patel <patelajeet1606@gmail.com>
2024-08-12 09:56:10 +0530

    implement rate limiting.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,21 +8,30 @@ import * as dotenv from "dotenv";
 import errorMiddleware from "./middlewares/error.middleware";
 import router from "./routes/router";
 import * as cookieParser from "cookie-parser";
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
 const app = express();
 
+export const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, 
+  max: 30,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many requests from this IP, please try again later."
+});
+app.use(limiter);
 app.use(cookieParser());
 
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: "application/json" }));
 
 const port = process.env.PORT || 80;
 
-app.use("/api/v1", router);
+app.use("/api/v1", limiter, router);
 
 app.use(errorMiddleware);
 
